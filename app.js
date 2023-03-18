c8f671e550618c34c7b8762d1d63a1f484ba5f33
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-18 21:48:27 +0200

    cors setup

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 let express = require("express")
 let app = express();
+const cors = require('cors');
 let PORT = process.env.PORT || 5555
 
 let bot = require("./bot")
 app.use("/bot", bot);
+
+const allowedOrigins = ['https://ball-shop-abibas.netlify.app/', 'http://localhost:3000'];
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
     console.log(`App listened on port ${PORT}`)
